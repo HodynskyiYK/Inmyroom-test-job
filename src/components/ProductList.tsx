fab6618e5bcd9c90b287dad39eb8201293573ac1
Yurii Hodynskyi <y.hodynskyi@eisgroup.com>
2022-01-18 21:54:58 +0200

    add new files

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react'
+import Product from './Product'
+
+const ProductList: FC = () => {
+    return (
+        <div>
+            <Product />
+        </div>
+    )
+}
+
+export default ProductList
